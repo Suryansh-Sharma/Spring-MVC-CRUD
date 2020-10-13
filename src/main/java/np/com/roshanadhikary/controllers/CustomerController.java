d7f8be38d3a56e517853ae5998d9332d7c9dc05d
Roshan <nahsorad@gmail.com>
2020-10-13 19:40:58 +0545

    Create a controller with mappings for get, list, and update

---
+++ src/main/java/np/com/roshanadhikary/controllers/CustomerController.java
@@ -0,0 +1,51 @@
+package np.com.roshanadhikary.controllers;
+
+import np.com.roshanadhikary.domain.Customer;
+import np.com.roshanadhikary.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CustomerController {
+    CustomerService customerService;
+
+    @Autowired
+    public void setCustomerService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/customer/{id}")
+    public String getCustomer(@PathVariable Integer id, Model model) {
+        model.addAttribute("customer", customerService.getCustomerById(id));
+        return "customer";
+    }
+
+    @RequestMapping("/customers")
+    public String listCustomers(Model model) {
+        model.addAttribute("customers", customerService.listCustomers());
+        return "customers";
+    }
+
+//    GET request to /customer/new opens up the customerForm
+    @RequestMapping("/customer/new")
+    public String addCustomer(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "customerForm";
+    }
+
+//    POST request to /customer persists the customer
+    @RequestMapping(value = "/customer", method = RequestMethod.POST)
+    public String saveOrUpdateCustomer(Customer customer) {
+        Customer savedCustomer = customerService.addOrUpdateCustomer(customer);
+        return "redirect:/customer/" + savedCustomer.getId();
+    }
+}
